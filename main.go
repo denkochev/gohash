3337b89e9494e5a27f3334f28bff25e13e71b74b
DenysWinPC <denkochev78@gmail.com>
2023-11-08 20:03:54 +0100

    feat: add examples

--- main.go
+++ main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
-	"crypto/sha1"
 	"fmt"
 	"gohash/hash"
 )
 
 func main() {
-	h := sha1.New()
-	h.Write([]byte("a"))
+	inputToHash := "You are in Denys git repo!"
 
-	hashH := fmt.Sprintf("%x", h.Sum(nil))
-	fmt.Println(hashH)
-	//fmt.Println(h.Sum(nil))
+	hasher := hash.Hash{}
+	hasher.New([]byte(inputToHash))
+	// this func returns 160-bit digest in [20]byte format
+	hashSlice := hasher.Get()
+	fmt.Println(hashSlice)
+	// you have 2 options get Hex digest
+	hexDigest := hasher.GetHex()
+	hexDigestRaw := fmt.Sprintf("%x", hashSlice)
+	fmt.Println(hexDigest)
+	fmt.Println(hexDigestRaw)
+	// [248 235 36 172 209 248 111 178 133 141 197 76 17 124 178 49 179 150 87 75]
+	// f8eb24acd1f86fb2858dc54c117cb231b396574b
+	// f8eb24acd1f86fb2858dc54c117cb231b396574b
 
-	//fmt.Println([]byte("are you?"))
-	//fmt.Println([]byte("are you?"))
+	directHash := hash.SHA1([]byte(inputToHash))
+	fmt.Printf("%x\n", directHash)
 
-	//hash.SHA1([]byte("are you?"))
-
-	// from http://book.itep.ru/6/sha1.htm
-	//hash.SHA1([]byte{97, 98, 99, 100, 101})
-	hash.SHA1([]byte("a"))
-
-	
+	// hash file
+	// path to desire file
+	hasher.NewFile("./hash/test_cases/go1.21.1.windows-amd64.msi")
+	hashForCompiler := hasher.Get()
+	fmt.Println(hashForCompiler)
+	fmt.Printf("%x\n", hashForCompiler)
+	// [36 125 64 190 9 175 100 155 166 165 202 206 51 199 145 54 114 22 193 49]
+	// 247d40be09af649ba6a5cace33c791367216c131
 }
